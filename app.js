e8c7e04f189858ff5318961c7498fae097e4e2d9
Yigit Efe Sozer <yigit.3f3@gmail.com>
2025-02-24 15:48:43 +0100

    db connection

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 // Import routes
 const productRoutes = require('./routes/products'); // Import product routes
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product')
 
 // Define the route for /products
-router.get('/', (req, res) => {
-    res.render('products');  // Render the 'products.ejs' file
+router.get('/', async(req, res) => {
+    const products = await Product.findOne({name: "Wireless Earbuds"})
+    res.render('products', {products});  
 });
+
+router.get('/:id', async(req, res) => {
+    const productId = req.params.id
+    console.log(req.params.id)
+    const products = await Product.findById(productId)
+    res.render('show', {products})
+})
 
 module.exports = router;
